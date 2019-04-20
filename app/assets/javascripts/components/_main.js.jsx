ccbb999c5231a19b9bd284cc94efb4036f0688ef
Chase <n.chase.spencer@gmail.com>
2019-04-20 19:56:48 -0400

    Home page created

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,16 @@
+const styles = {
+    margin: "1em",
+    padding: "1em",
+    border: "1px solid #ddd"
+};
+
+const Main = props => {
+    return (
+        <div style={styles}>
+            <div>
+                <h1>React RoR Blog</h1>
+            </div>
+            <p> here we will add our article CRUD component </p>
+        </div>
+    );
+};
